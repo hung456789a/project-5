464ca96a8b8284f4eee457fedcd8d1487ac7a2ba
hung456789a <hung456789a@gmail.com>
2025-05-07 14:28:50 +0700

    update

--- logger.js
+++ logger.js
@@ -3,15 +3,17 @@ const { LogstashTransport } = require('winston-logstash-transport');
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(), // Logstash prefers JSON
+  format: winston.format.json(),
   transports: [
     new LogstashTransport({
       port: 5000,
-      node_name: 'my-node-app',
-      host: 'logstash'
+      host: 'logstash',              // Tên service hoặc container Logstash
+      node_name: 'logstash',
+      max_connect_retries: 10,       // ✅ Tự động thử lại kết nối
+      timeout_connect_retries: 10000 // ✅ chờ tối đa 10s mỗi lần
     }),
-    new winston.transports.Console({          // ✅ thêm transport console
-      format: winston.format.simple()         // hoặc format khác bạn muốn
+    new winston.transports.Console({
+      format: winston.format.simple()
     })
   ]
 });
