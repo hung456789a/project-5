b4270aeb3d8e6b99a9e6331fbb38892bb175a653
hung456789a <hung456789a@gmail.com>
2025-05-07 12:16:24 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-const http = require('http'); 
-const winston = require('winston'); //library for logstash
-const { LogstashTransport } = require('winston-logstash-transport');  //library for logstash
+// const http = require('http'); 
+// const winston = require('winston'); //library for logstash
+// const { LogstashTransport } = require('winston-logstash-transport');  //library for logstash
 const express = require('express');     // libary for mongodb connection
 const mongoose = require('mongoose');   // libary for mongodb connection
 const bodyParser = require('body-parser');   // libary for mongodb connection
@@ -12,32 +12,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', taskRoutes);
 
-//  Connect to mongodb
+ Connect to mongodb
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(3000, () => console.log('Server running on http://localhost:3000'));
   })
   .catch(err => console.log(err));
-//logstash
-const logger = winston.createLogger({
-  transports: [
-    new LogstashTransport({
-      port: 5000,
-      node_name: 'my-node-app',
-      host: 'localhost' // địa chỉ Logstash
-    })
-  ]
-});
+// //logstash
+// const logger = winston.createLogger({
+//   transports: [
+//     new LogstashTransport({
+//       port: 5000,
+//       node_name: 'my-node-app',
+//       host: 'logstash' // địa chỉ Logstash
+//     })
+//   ]
+// });
 
-const server = http.createServer((req, res) => {
-  logger.info('Received request', { url: req.url });
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!\n');
-});
+// const server = http.createServer((req, res) => {
+//   logger.info('Received request', { url: req.url });
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, world!\n');
+// });
 
-server.listen(3000, () => {
-  logger.info('Server started on port 3000');
-  console.log('Server running at http://localhost:3000/');
-});
-//logstash
+// server.listen(3000, () => {
+//   logger.info('Server started on port 3000');
+//   console.log('Server running at http://localhost:3000/');
+// });
+// //logstash
