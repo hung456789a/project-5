11219772871b3037ceb0d11ee040ba01e0aa3b59
hung456789a <hung456789a@gmail.com>
2025-05-09 10:09:44 +0700

    update

--- app.js
+++ app.js
@@ -24,26 +24,3 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => {
     logger.error('❌ MongoDB connection failed', { error: err});
     console.log(err)});
-// //logstash
-// const logger = winston.createLogger({
-//   transports: [
-//     new LogstashTransport({
-//       port: 5000,
-//       node_name: 'my-node-app',
-//       host: 'logstash' // địa chỉ Logstash
-//     })
-//   ]
-// });
-
-// const server = http.createServer((req, res) => {
-//   logger.info('Received request', { url: req.url });
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello, world!\n');
-// });
-
-// server.listen(3000, () => {
-//   logger.info('Server started on port 3000');
-//   console.log('Server running at http://localhost:3000/');
-// });
-// //logstash
